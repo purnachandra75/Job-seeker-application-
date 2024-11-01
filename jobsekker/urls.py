1b27049666f9c4a3fc181bc78603d5822e0d4e5d
purnachandra75 <143241294+purnachandra75@users.noreply.github.com>
2024-11-01 18:24:16 +0530

    Add files via upload

---
+++ jobsekker/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for jobsekker project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+router=DefaultRouter()
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('myapp.urls')),
+    path('',include('job_seekers.urls')),
+    path('',include('Employeers.urls')),
+    path('api-auth/',include('rest_framework.urls')),
+    path('dj_rest_auth/',include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/',include('dj_rest_auth.registration.urls')),
+    path('',include(router.urls))
+]
